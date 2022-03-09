6ed2d55c9f6c0a4aece9464d4d0ffcacab047d07
Khoa Quach <khoaquachschool@gmail.com>
2022-03-08 17:14:12 -0800

    aesthetics additions

--- crazy_bird.js
+++ crazy_bird.js
@@ -1,9 +1,10 @@
 import {defs, tiny} from './examples/common.js';
 
 const {
-    Vector, Vector3, vec, vec3, vec4, color, hex_color, Shader, Matrix, Mat4, Light, Shape, Material, Scene,
+    Vector, Vector3, vec, vec3, vec4, color, hex_color, Shader, Matrix, Mat4, Light, Shape, Material, Scene, Texture
 } = tiny;
 
+const {Textured_Phong} = defs
 class Cube extends Shape {
     constructor() {
         super("position", "normal",);
@@ -67,9 +68,20 @@ class Base_Scene extends Scene {
 
         // *** Materials
         this.materials = {
+            flame: new Material(new Textured_Phong(), {
+                //color: hex_color("#ff3939"),
+                ambient: 1, diffusivity: 0.1, specularity: 0.1,
+                texture: new Texture("assets/hell.jpg")
+            }),
+            texture_plastic: new Material(new Textured_Phong(),
+                {ambient: .4, diffusivity: .6, color: hex_color("#50a127"), texture: new Texture("assets/green_texture.jpg")}),
             plastic: new Material(new defs.Phong_Shader(),
-                {ambient: .4, diffusivity: .6, color: hex_color("#ffffff")}),
-            plasticlose: new Material(new defs.Phong_Shader(),
+                {ambient: 0.7, diffusivity: 1.0, specularity: 1.0, color: hex_color("#8c0b0b")}),
+            bird: new Material(new defs.Phong_Shader(),
+                {ambient: 1.0, diffusivity: 0.5, specularity: 1.0, color: hex_color("#ffcb02")}),
+            //plastic: new Material(new defs.Phong_Shader(),
+            //   {ambient: .4, diffusivity: .6, color: hex_color("#ffffff")}),
+            plastic_close: new Material(new defs.Phong_Shader(),
                 {ambient: .4, diffusivity: .6, color: hex_color("#ffffff")}),
         };
         // The white material and basic shader are used for drawing the outline.
@@ -78,7 +90,7 @@ class Base_Scene extends Scene {
         this.third_person_camera = Mat4.translation(5, -15, -50);
         this.change_view = false;
         this.pipes = [{Matrix:Mat4.identity().times(Mat4.translation(0,-2.5,-20)),height:this.randomNumber(4,12)}];
-        
+
     }
 
     display(context, program_state) {
@@ -90,7 +102,7 @@ class Base_Scene extends Scene {
             this.children.push(context.scratchpad.controls = new defs.Movement_Controls());
             // Define the global camera and projection matrices, which are stored in program_state.
             program_state.set_camera(this.initial_camera_location);
-            
+
         }
         program_state.projection_transform = Mat4.perspective(
             Math.PI / 4, context.width / context.height, 1, 100);
@@ -100,11 +112,11 @@ class Base_Scene extends Scene {
         program_state.lights = [new Light(light_position, color(1, 1, 1, 1), 1000)];
     }
 
-    randomNumber(min, max) { 
+    randomNumber(min, max) {
         min = Math.ceil(min);
         max = Math.floor(max);
         return Math.floor(Math.random() * (max - min + 1)) + min;
-    } 
+    }
 }
 
 export class CrazyBird extends Base_Scene {
@@ -114,7 +126,7 @@ export class CrazyBird extends Base_Scene {
      * This gives you a very small code sandbox for editing a simple scene, and for
      * experimenting with matrix transformations.
      */
-    
+
     set_colors() {
         // TODO:  Create a class member variable to store your cube's colors.
         // Hint:  You might need to create a member variable at somewhere to store the colors, using `this`.
@@ -122,7 +134,7 @@ export class CrazyBird extends Base_Scene {
     }
 
     make_control_panel() {
-        
+
         this.key_triggered_button("Jump", ["b"], () => {
             this.force = 0.6;
         });
@@ -135,14 +147,14 @@ export class CrazyBird extends Base_Scene {
     }
 
     draw_box(context, program_state, model_transform, height) {
-        
+
         const green = hex_color("#57c94f");
         const gap = 4;
         const height_limit = 20;
 
         // draw bottom pipe
         for (let i=1; i<height; i++){
-            this.shapes.pipe.draw(context, program_state, model_transform, this.materials.plastic.override({color:green}))
+            this.shapes.pipe.draw(context, program_state, model_transform, this.materials.plastic)
             model_transform = model_transform.times(Mat4.translation(0,2,0));
         }
 
@@ -152,7 +164,7 @@ export class CrazyBird extends Base_Scene {
         // draw top pipe
         let top_count = height_limit - height - gap;
         for (let i=1; i<top_count; i++){
-            this.shapes.pipe.draw(context, program_state, model_transform, this.materials.plastic.override({color:green}))
+            this.shapes.pipe.draw(context, program_state, model_transform, this.materials.plastic)
             model_transform = model_transform.times(Mat4.translation(0,2,0));
         }
     }
@@ -170,8 +182,8 @@ export class CrazyBird extends Base_Scene {
 
         let model_transform_bird = Mat4.identity().times(Mat4.translation(0,5,10));
 
-        
-        
+
+
         if (!this.lost){
             model_transform_bird = model_transform_bird.times(Mat4.translation(0,this.x,0));
             let g = -0.015 + this.force;
@@ -229,11 +241,11 @@ export class CrazyBird extends Base_Scene {
         if (this.pipes.length > 0 && this.pipes[0].Matrix[2][3] > 50){
             this.pipes.shift();
         }
-        
+
         // draw floor
         let floor_transform = Mat4.identity().times(Mat4.translation(-12, -4, 0)).times(Mat4.scale(50, 0.5, 100));
-        this.shapes.cube.draw(context, program_state, floor_transform, this.materials.plasticlose);
-        
+        this.shapes.cube.draw(context, program_state, floor_transform, this.materials.flame);
+
         //if bird hits the floor or hits a pillar, game over
         //floor has y value of -7.5, sphere has radius of 1
         if (this.x <= -7.5){
@@ -241,17 +253,64 @@ export class CrazyBird extends Base_Scene {
             this.lost = true;
             this.prevLocation = model_transform_bird;
         }
-            //this.shapes.bird.draw(context, program_state, model_transform_bird, this.materials.plastic.override({color:blue}));
-            //this.shapes.bird.draw(context, program_state, model_transform_bird.times(Mat4.translation(0, 1, 0)), this.materials.plastic.override({color:blue}));
-       
+        //this.shapes.bird.draw(context, program_state, model_transform_bird, this.materials.plastic.override({color:blue}));
+        //this.shapes.bird.draw(context, program_state, model_transform_bird.times(Mat4.translation(0, 1, 0)), this.materials.plastic.override({color:blue}));
 
-        
+
+
         // draw bird
-        this.shapes.bird.draw(context, program_state, model_transform_bird, this.materials.plastic.override({color:blue}));
-        
+        this.shapes.bird.draw(context, program_state, model_transform_bird, this.materials.bird);
+
         // reset force from keyboard press
         this.force = 0;
-        
-        
+
+
     }
 }
+
+
+
+// Extra
+class Texture_Scroll_X extends Textured_Phong {
+    // TODO:  Modify the shader below (right now it's just the same fragment shader as Textured_Phong) for requirement #6.
+    fragment_glsl_code() {
+        return this.shared_glsl_code() + `
+            varying vec2 f_tex_coord;
+            uniform sampler2D texture;
+            uniform float animation_time;
+            
+            void main(){
+                // COURTESY OF DISCUSSION 1C SKELETON CODE :D !
+                // Sample the texture image in the correct place:
+                float scale_factor = -2.0 * mod(animation_time, 4.0); // rotation needs 4 so it isn't skippy, but it is lenient for texture scroll
+                vec2 new_tex_coord = vec2(f_tex_coord.x + scale_factor, f_tex_coord.y); // scroll x axis, to the right
+                vec4 tex_color = texture2D( texture, new_tex_coord);
+                
+                // Modified from TA demo code
+                float u = mod(new_tex_coord.x, 1.0);
+                float v = mod(new_tex_coord.y, 1.0);
+                if ((u >= 0.75 && u <= 0.85) && (v <= 0.85 && v >= 0.15))
+                {
+                    tex_color = vec4(0, 0, 0, 1.0);
+                }
+                if ((u >= 0.15 && u <= 0.25) && (v <= 0.85 && v >= 0.15))
+                {
+                    tex_color = vec4(0, 0, 0, 1.0);
+                }
+                if ((v >= 0.75 && v <= 0.85) && (u <= 0.85 && u >= 0.15))
+                {
+                        tex_color = vec4(0, 0, 0, 1.0);
+                }
+                if ((v >= 0.15 && v <= 0.25)&& (u <= 0.85 && u >= 0.15))
+                {
+                        tex_color = vec4(0, 0, 0, 1.0);
+                }
+                if( tex_color.w < .01 ) discard;
+                                                                         // Compute an initial (ambient) color:
+                gl_FragColor = vec4( ( tex_color.xyz + shape_color.xyz ) * ambient, shape_color.w * tex_color.w ); 
+                                                                         // Compute the final color with contributions from lights:
+                gl_FragColor.xyz += phong_model_lights( normalize( N ), vertex_worldspace );
+        } `;
+    }
+}
+
